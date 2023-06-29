38445048d412c14b92f75d6c900cfaa3c306a08a
R0BC0D3R <R0BC0D3R@gmail.com>
2023-06-29 15:43:21 -0400

    Add project files.

---
+++ Enums/ExampleEnum.cs
@@ -0,0 +1,14 @@
+﻿using Discord.Interactions;
+
+namespace NervaDiscordBot.Enums
+{
+    public enum ExampleEnum
+    {
+        First,
+        Second,
+        Third,
+        Fourth,
+        [ChoiceDisplay("Twenty First")]
+        TwentyFirst
+    }
+}
